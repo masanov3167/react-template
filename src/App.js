6bd1dc4e0a73359c846d2b3859310621f3e10e3b
Masanov <masanov3167@gmail.com>
2022-04-21 16:49:36 +0500

    template

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1 className="visually-hidden"> Hello world</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
